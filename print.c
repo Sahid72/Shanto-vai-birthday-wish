3b85f1def1802f9861cedfcaea592b25350e867c
Sahid72e <sahidahamed72@gmail.com>
2021-09-30 18:26:19 +0600

    update

---
+++ print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("My name is Sahid \n");
+    printf("I didn't know the name \nTwinkle cats \n");
+    printf("%d\n", 4+4);
+    return 0;
+}
---
+++ script.js
@@ -0,0 +1,14 @@
+let count = 3;
+
+switch(count){
+    case 1: 
+    console.log("Number is 1");
+    break;
+
+    case >3: 
+    console.log("Number is 3");
+    break;
+    default:
+        console.log("Number is negative")
+}
+return 0;
